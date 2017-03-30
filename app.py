f65fe6250463f04135a899957114aca02f34443c
Rahul Agrawal <rahulagrawal@0587358510.wireless.umich.net>
2017-03-30 14:20:41 -0400

    gets and returns csv based on criteria

--- app.py
+++ app.py
@@ -37,6 +37,12 @@ def send_text_file(file_name):
     return app.send_static_file(file_dot_text)
 '''
 
+@app.route("/<file_name>.txt")
+def send_text_file(file_name):
+
+    file_dot_text = file_name + ".txt"
+    return app.send_static_file(file_dot_text)
+
 '''
     expects a query string of the form
     ?
@@ -51,6 +57,100 @@ def send_text_file(file_name):
     ###     if you're creative enough pandas has an SQL queiries feature
     ###     which might be useful; however, let me know if you need help
 
+def treat_query(query_str):
+
+    '''
+        expects a query string of the form
+        ?
+            PDB_IDS=<> a list of all the target PDB_IDS comman separated; if empty will assume all in databank
+            keys=<> a list of all the target atoms in the isoalloxezene; if none, will assume all
+
+    '''
+
+    # seperates the query string into the two criteria PDB_IDS and keys
+    crit_list = query_str.split('\n')
+    pdb_crit = crit_list[0]
+    keys_crit = crit_list[1]
+
+    # gets path for reading in csv
+    folder_path = os.getcwd()
+    data_path = os.path.join(folder_path, "data.csv")
+
+    # gets path for writing the csv file later
+    file_out_path = os.path.join(folder_path, "output.csv")
+
+    # gets csv file_name
+    data_pd = pd.read_csv(data_path)
+
+    # initializes variables to be used later
+    pdb_ids_str = ""
+    pdb_id_list = []
+
+    keys_str = ""
+    keys_list = []
+
+    df_list = []
+
+    if ((pdb_crit == "PDB_IDS=<>") and (keys_crit == "keys=<>")):
+        return app.send_static_file("data.csv")
+    elif ((pdb_crit != "PDB_IDS=<>") and (keys_crit != "keys=<>")):
+
+        # gets a list of the relevant pdb ids
+        pdb_ids_str = pdb_crit[9:-1]
+        pdb_id_list = pdb_ids_str.split(',')
+
+        # gets a list of the relevant keys
+        keys_str = keys_crit[6:-1]
+        keys_list = keys_str.split(',')
+
+        # gets all relevant rows based on pdb_id and key atoms
+        for pdb_key in pdb_id_list:
+            for key_id in keys_list:
+                df_list.append(data_pd[(data_pd.target_atom_name == key_id) & (data_pd.PDB_ID == pdb_key)])
+
+        # print(df_list)
+
+        # combines all relevant dataframes into one
+        df_final = pd.concat(df_list)
+
+        # turns dataframe into csv file
+        df_final.to_csv(path_or_buf=file_out_path)
+
+        return None
+    elif ((pdb_crit != "PDB_IDS=<>") and (keys_crit == "keys=<>")):
+
+        # gets a list of the relevant pdb ids
+        pdb_ids_str = pdb_crit[9:-1]
+        pdb_id_list = pdb_ids_str.split(',')
+
+        # makes a list of dataframes for rows that have relvant pdb_id
+        for pdb_key in pdb_id_list:
+            df_list.append(data_pd[data_pd.PDB_ID == pdb_key])
+
+        # combines all relevant dataframes into one
+        df_final = pd.concat(df_list)
+
+        # turns dataframe to csv file
+        df_final.to_csv(path_or_buf=file_out_path)
+
+        return app.send_static_file("output.csv")
+    else:
+
+        # gets a list of the relevant keys
+        keys_str = keys_crit[6:-1]
+        keys_list = keys_str.split(',')
+
+        # makes a list of dataframes from rows that have relevant keys
+        for key_id in keys_list:
+            df_list.append(data_pd[data_pd.target_atom_name == key_id])
+
+        # appends all the dataframes to the first
+        df_final = pd.concat(df_list)
+
+        # turns dataframe to csv file
+        df_final.to_csv(path_or_buf=file_out_path)
+
+        return app.send_static_file("output.csv")
     # incomplete
     return render_template('404.html'), 404
 
